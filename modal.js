0e0f437ec040d61f3f05d24a266ddb7264a2962c
Andrii_K. <01birofe01@gmail.com>
2025-04-18 23:24:16 +0300

    modalwindowfail

---
+++ modal.js
@@ -0,0 +1,17 @@
+let modal = document.getElementById("modalWindow");
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
+setTimeout(openModal, 100000)
