3b78d69b810b4260428332c800b32f69be2abfeb
Andrii_K. <01birofe01@gmail.com>
2025-04-20 12:11:45 +0300

    prettier

--- modal.js
+++ modal.js
@@ -1,27 +1,27 @@
 let modal = document.getElementById("modalWindow");
 
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 window.onclick = function (event) {
-    if (event.target == modal) {
-        closeModal();
-    }
-}
+  if (event.target == modal) {
+    closeModal();
+  }
+};
 
 // setTimeout(openModal, 60000)
 
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      modal.style.display = "none";
-    }
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    modal.style.display = "none";
+  }
 });
-  
+
 // let mobMenu = document.getElementById("menu");
 
 // function openMobMenu() {
