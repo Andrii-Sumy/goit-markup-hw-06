ba78c8e2384668dc81df6d1cbd67a9ac696dc0ac
Andrii_K. <01birofe01@gmail.com>
2025-04-20 12:09:16 +0300

    laststepmobmenubtnjs

--- modal.js
+++ modal.js
@@ -20,4 +20,19 @@ document.addEventListener('keydown', (event) => {
     if (event.key === 'Escape') {
       modal.style.display = "none";
     }
-  });
+});
+  
+// let mobMenu = document.getElementById("menu");
+
+// function openMobMenu() {
+//     mobMenu.style.display = "block";
+// }
+
+// function closeMobMenu() {
+//     mobMenu.style.display = "none";
+// }
+// window.onclick = function (event) {
+//     if (event.target == mobMenu) {
+//         closeMobMenu();
+//     }
+// }
